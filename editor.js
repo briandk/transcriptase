402a082aab89c81f7569c999660a49accbbdcdfe
Brian Danielak <briandaniela.k+github@gmail.com>
2016-11-04 23:34:11 -0400

    Initialize quill editor in the render process

--- editor.js
+++
@@ -1,8 +0,0 @@
-// Initialize Quill editor
-let quill = new Quill('#editor', {
-  modules: {
-    syntax: false,              // Include syntax module
-    toolbar: true  // Include button in toolbar
-  },
-  theme: 'snow'
-})
