1ba8e91c442f6fcefb26fb285682ea0096d82d1d
Brian A. Danielak <briandaniela.k+github@gmail.com>
2017-06-15 14:40:10 -0400

    Try and fail generating an ANTLR CSV grammar
    I think this road is too complicated, and we should try a JISON-based parser instead.
    
    https://zaa.ch/jison/

--- grammar/CSV/testrungrammar.js
+++ grammar/CSV/testrungrammar.js
@@ -1,4 +1,8 @@
 let fs = require('fs')
+let antlr4 = require('antlr4')
+let CSVLexer = require('./CSVLexer')
+let CSVParser = require('./CSVParser')
+let myParser = null
 
 fs.readFile(
   'data.csv', { encoding: 'utf-8' },
@@ -6,16 +10,16 @@ fs.readFile(
     if (error) {
       console.log(error)
     } else {
-      console.log(data)
+      testCSVgrammar(data)
     }
   }
 )
 
-// let input = "your text to parse here"
-// let chars = CharStreams.fromString(input);
-// let lexer = new MyGrammarLexer.MyGrammarLexer(chars);
-// let tokens  = new antlr4.CommonTokenStream(lexer);
-// let parser = new MyGrammarParser.MyGrammarParser(tokens);
-// parser.buildParseTrees = true;
-// let tree = parser.MyStartRule();
-
+const testCSVgrammar = function(fileInput) {
+  let chars = new antlr4.CharStreams.fromString(fileInput)
+  let lexer = new CSVLexer.CSVLexer(chars)
+  let tokens = new antlr4.CommonTokenStream(lexer)
+  let parser = new CSVParser.CSVParser(tokens)
+  parser.buildParseTrees = true
+  let tree = parser.file()
+}
