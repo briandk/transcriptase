a67a525dd4da67a5051cc239cea91fbbf3e46147
Brian A. Danielak <briandaniela.k+github@gmail.com>
2017-06-23 15:31:32 -0400

    Make bracket regexp a constant

--- renderer-process/RegexExtraction.js
+++ renderer-process/RegexExtraction.js
@@ -1,10 +1,9 @@
-let bracketPattern = /\[([^\]]+)\]/g
-let inputText = "To [sleep], [per]chance []to [dream]"
+const bracketPattern = /\[([^\]]+)\]/g
+const inputText = "To [sleep], [per]chance []to [dream]"
 
 let match
 let startingIndex
 let matchLength
-let quillOperations = []
 
 while (match = bracketPattern.exec(inputText)) {
   startingIndex = match.index + 1   // Have to advance past the opening square bracket
