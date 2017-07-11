f33fffdf4bc1ee067fc0a05d23b5610114fe20fd
Brian A. Danielak <briandaniela.k+github@gmail.com>
2017-07-11 10:40:50 -0400

    Try recursively matching timestamps

--- renderer-process/RegexExtraction.js
+++ renderer-process/RegexExtraction.js
@@ -1,17 +1,54 @@
 const bracketPattern = /\[([^\]]+)\]/g
-const inputText = "To [sleep], [per]chance []to [dream]"
+const inputText = 'To [sleep], [per]chance [to] [dream]'
 
-let match
-let startingIndex
-let matchLength
+const matchTimestampText = function (inputText) {
+  const lengthOfDelimiter = 1 // Have to advance past/before the opening/closing brackets
+  let match
+  let startingIndex
+  let matchLength
 
-while (match = bracketPattern.exec(inputText)) {
-  startingIndex = match.index + 1   // Have to advance past the opening square bracket
-  matchLength = bracketPattern.lastIndex - startingIndex - 1  // and remove the closing bracket
-  console.log(
-    `starting index: ${startingIndex}, matchLength: ${matchLength}
-     ${inputText.slice(startingIndex, startingIndex + matchLength)}
-     ${match}
-    `
-  )
+  while (match = bracketPattern.exec(inputText)) {
+    startingIndex = (match.index + lengthOfDelimiter)
+    matchLength = (bracketPattern.lastIndex - startingIndex - lengthOfDelimiter)
+    console.log(
+      `starting index: ${startingIndex}, matchLength: ${matchLength}
+       ${inputText.slice(startingIndex, startingIndex + matchLength)}
+       ${match}
+      `
+    )
+  }
 }
+
+const recursivelyMatchTimestamps =
+  function (pattern, inputText, finalIndexOfLastMatch, operations) {
+    const match = pattern.exec(inputText)
+    if (match === null) {
+      console.log(operations)
+      return (
+        operations.concat(
+          { retain: inputText.slice(finalIndexOfLastMatch).length }
+      ))
+    } else {
+      const lengthOfDelimiter = 1
+      const matchLength = pattern.lastIndex - match.index
+      const timestampLength = matchLength - (2 * lengthOfDelimiter)
+
+      recursivelyMatchTimestamps(
+        pattern,
+        inputText,
+        pattern.lastIndex,
+        operations.concat([
+          { retain: match.index + lengthOfDelimiter },
+          { retain: timestampLength, attributes: { link: true } },
+          { retain: lengthOfDelimiter }
+        ])
+      )
+    }
+  }
+
+recursivelyMatchTimestamps(
+  bracketPattern,
+  inputText,
+  0,
+  []
+)
