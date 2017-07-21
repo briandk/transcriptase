467dd1021709d05ddaa5e240677e91565f9ad255
Brian A. Danielak <briandaniela.k+github@gmail.com>
2017-07-21 14:42:14 -0400

    Make timestamps seek video to their timecodes

--- renderer-process/scrubVideoToTimestamp.js
+++ renderer-process/scrubVideoToTimestamp.js
@@ -1,14 +1,20 @@
 const moment = require('moment')
 
 const scrubVideoToTimestamp = function () {
-  let player = document.querySelector('.video-js')
-  const timeToGoTo = moment.duration(this.innerHTML).asSeconds()
-  console.log(this.innerHTML)
-  console.log(player)
+  let player = document.getElementsByTagName('video')[0]
+  let timestamp = this.innerHTML
+  const timeToGoTo = moment.duration({
+    seconds: timestamp.split(':')[1],
+    hours: timestamp.split(':')[0]
+  }).asSeconds()
 
-  if (player !== null && player.duration) {
-    if (timeToGoTo >= 0 && timeToGoTo <= player.duration()) {
-      player.currentTime(timeToGoTo)
+  console.log(timestamp)
+  console.log(timeToGoTo)
+
+  if (player !== null && player.duration !== null) {
+    if (timeToGoTo >= 0 && timeToGoTo <= player.duration) {
+      console.log('reached inner condition')
+      player.currentTime = timeToGoTo
     }
   }
 }
