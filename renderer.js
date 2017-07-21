5736f4e1d38b32236d7164c3909d0b4c264e6ba9
Brian A. Danielak <briandaniela.k+github@gmail.com>
2017-07-20 20:08:12 -0400

    Try creating a placeholder panel before video loads

--- renderer.js
+++ renderer.js
@@ -47,7 +47,8 @@ ipc.on('saved-file', (event, savePath) => {
   setIsEditorDirty(false)
 })
 
-createVideoPlayer(videoContainer) // create the first (blank) instance of the video player
+// create the first (blank) instance of the video player
+// createVideoPlayer(videoContainer)
 
 setInterval(
   () => { console.log(isEditorDirty()) },
