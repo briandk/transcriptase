6e2a0c8b7440e1701b08188e60f38d29a9d42e16
Brian A. Danielak <briandaniela.k+github@gmail.com>
2017-08-04 15:46:51 -0400

    Delete unhelpful timestamp spec
    It doesn't test anything useful

--- spec/insertCurrentTimeSpec.js
+++ spec/insertCurrentTimeSpec.js
@@ -3,12 +3,3 @@
 //     expect(true).toBe(true);
 //   });
 // });
-
-describe('Inserting the current time', function () {
-  it('converts duration in seconds to a formatted time string', function () {
-    const { formatDuration } = require('./../renderer-process/insertCurrentTime')
-    const moment = require('moment')
-    const duration1 = moment.duration(144, 'seconds')
-    expect(formatDuration(duration1)).toBe('02:24')
-  })
-})
