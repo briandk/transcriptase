6c71be97590b510520e60181ab730aa568eecae5
Brian Danielak <briandaniela.k+github@gmail.com>
2017-12-22 20:09:30 -0500

    Stub a basic spectron setup
    I hate Spectron because the commented-out code *will* run, and it will pass this test.
    
    It should not pass this test. At all.

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,15 +1,18 @@
 /* eslint-env jasmine */
 
 describe('Launching the app', () => {
-  it('creates a visible window', async () => {
+  it('creates a visible window', () => {
     const Application = require('spectron').Application
     let app = new Application({
       path: 'node_modules/.bin/electron',
       args: ['built/index.js']
     })
-    await app.start()
+
+    // app.start()
+    //   .then(() => { return app.browserWindow.isVisible() })
+    //   .then((isVisible) => { expect(isVisible).toBe('red') })
+    //   .then(() => { return app.stop() })
     // expect(app.browserWindow.isVisible()).toBe(true)
-    // expect(false).toBe(true)
     // return app.browserWindow.isVisible()
   })
 })
