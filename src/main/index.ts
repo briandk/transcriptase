c1e89a106bda9c4a81f2d4f25300ebfac78a2a35
Brian Danielak <briandaniela.k+github@gmail.com>
2018-02-02 21:08:05 -0500

    Fix tslint errors main index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,116 +1,120 @@
-let { app, BrowserWindow, Menu } = require('electron')
-const localShortcut = require('electron-localshortcut')
-const fs = require('fs-plus')
-const ipc = require('electron').ipcMain
-const { saveFile } = require('./saveTranscript')
-const { showUnsavedChangesDialog } = require('../common/closeTheApp')
-const menuTemplate = require('./menu/menuTemplate')
-const { registerPlayPauseToggleAsGlobalShortcut } = require('./main-process/controlPlayback')
-const { registerJumpBackNSeconds } = require('./main-process/controlPlayback')
-const electronLocalShortcut = require('electron-localshortcut')
-const { autoUpdater } = require('electron-updater')
+import { showUnsavedChangesDialog } from "../common/closeTheApp";
+import { saveFile } from "../common/saveTranscript";
+import {
+  app,
+  BrowserWindow,
+  ipcMain as ipc,
+  Menu,
+} from "electron";
 
-let mainWindow
+import * as fs from "fs";
+import { template as menuTemplate } from "../menu/menuTemplate";
+import { registerPlayPauseToggleAsGlobalShortcut } from "./controlPlayback";
+import { registerJumpBackNSeconds } from "./controlPlayback";
+import * as electronLocalShortcut from "electron-localshortcut";
+import { autoUpdater } from "electron-updater";
+import * as showFileSelectionDialog from "./showFileSelectionDialog";
 
-function createWindow () {
+let mainWindow;
+
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 900,
     height: 600,
     show: false,
     frame: true,
-    title: 'Transcriptase'
-  })
+    title: "Transcriptase",
+  });
 
-  app.on('will-quit', function () {
-    localShortcut.unregisterAll(mainWindow)
-  })
+  app.on("will-quit", () => {
+    electronLocalShortcut.unregisterAll(mainWindow);
+  });
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
   // mainWindow.webContents.openDevTools()   // Open the DevTools.
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 
-  mainWindow.on('close', (event) => {
-    event.preventDefault()
-    mainWindow.webContents.send('user-wants-to-close-the-app')
-  })
+  mainWindow.on("close", (event) => {
+    event.preventDefault();
+    mainWindow.webContents.send("user-wants-to-close-the-app");
+  });
 }
 
-app.on('ready', () => {
-  createWindow()
-  registerPlayPauseToggleAsGlobalShortcut(mainWindow, electronLocalShortcut.register)
-  registerJumpBackNSeconds(mainWindow, electronLocalShortcut.register)
-  autoUpdater.checkForUpdatesAndNotify()
+app.on("ready", () => {
+  createWindow();
+  registerPlayPauseToggleAsGlobalShortcut(mainWindow, electronLocalShortcut.register);
+  registerJumpBackNSeconds(mainWindow, electronLocalShortcut.register);
+  autoUpdater.checkForUpdatesAndNotify();
 
-  mainWindow.once('ready-to-show', () => {
-    const menu = Menu.buildFromTemplate(menuTemplate)
-    Menu.setApplicationMenu(menu)
-    mainWindow.show()
-  })
+  mainWindow.once("ready-to-show", () => {
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+    mainWindow.show();
+  });
 
-})
+});
 
-app.on('window-all-closed', function () {
-  app.quit()
-})
+app.on("window-all-closed", () => {
+  app.quit();
+});
 
-app.on('activate', function () {
+app.on("activate", () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // File selection
-const showFileSelectionDialog = require('./main-process/showFileSelectionDialog')
-ipc.on('open-file-dialog', function (event, roleOfFile) {
-  let file = showFileSelectionDialog(event, roleOfFile)
+ipc.on("open-file-dialog", (event, roleOfFile) => {
+  const file = showFileSelectionDialog(event, roleOfFile);
   if (file) {
-    event.sender.send('a-file-was-selected', file.toString(), roleOfFile)
+    event.sender.send("a-file-was-selected", file.toString(), roleOfFile);
   }
-})
+});
 
-ipc.on('read-transcript-from-filepath', (event, filePath) => {
-  let transcriptStream = fs.createReadStream(
-    filePath.toString(), { encoding: 'utf-8' }
-  )
-  let data = ''
+ipc.on("read-transcript-from-filepath", (event, filePath) => {
+  const transcriptStream = fs.createReadStream(
+    filePath.toString(), { encoding: "utf-8" },
+  );
+  let data = "";
 
-  transcriptStream.on('data', (chunk) => { data += chunk })
-  transcriptStream.on('end', () => {
-    event.sender.send('transcript-was-read-from-file', data, filePath.toString())
-  })
+  transcriptStream.on("data", (chunk) => { data += chunk; });
+  transcriptStream.on("end", () => {
+    event.sender.send("transcript-was-read-from-file", data, filePath.toString());
+  });
 
   // fs.readFile(
   //   filePath.toString(),
-  //   'utf-8',
+  //   "utf-8",
   //   (err, data) => {
   //     if (err) console.log(err)
   //     console.log(data)
-  //     event.sender.send('transcript-was-read-from-file', data, filePath.toString())
+  //     event.sender.send("transcript-was-read-from-file", data, filePath.toString())
   //   }
   // )
-})
+});
 
 // file saving
-ipc.on('save-transcript', (event, transcriptText, lastSavedPath) => {
-  saveFile(event, transcriptText, lastSavedPath, mainWindow)
-})
+ipc.on("save-transcript", (event, transcriptText, lastSavedPath) => {
+  saveFile(event, transcriptText, lastSavedPath, mainWindow);
+});
 
-ipc.on('show-unsaved-changes-dialog', (event, transcriptEditor, lastSavedPath) => {
-  showUnsavedChangesDialog(event, mainWindow, transcriptEditor, lastSavedPath)
-})
+ipc.on("show-unsaved-changes-dialog", (event, transcriptEditor, lastSavedPath) => {
+  showUnsavedChangesDialog(event, mainWindow, transcriptEditor, lastSavedPath);
+});
 
-ipc.on('its-safe-to-close-the-app', (event) => {
-  mainWindow.destroy()
-})
+ipc.on("its-safe-to-close-the-app", (event) => {
+  mainWindow.destroy();
+});
 
 // global shortcuts
 
-// app.on('ready', () => {
-//   globalShortcut.register('CmdOrCtrl+;', function () {
+// app.on("ready", () => {
+//   globalShortcut.register("CmdOrCtrl+;", function () {
 //     console.log(`inserting `)
 //   })
 // })
