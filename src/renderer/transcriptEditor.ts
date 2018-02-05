ac0c905c8b9f1ca2ff9a8b7ebad1b82be96a3ba3
Brian Danielak <briandaniela.k+github@gmail.com>
2018-02-05 18:16:34 -0500

    Fix my rename screw-up

---
+++ src/renderer/transcriptEditor.ts
@@ -0,0 +1,34 @@
+import { Quill } from "quill";
+
+const formatTimestampsOnTextChange = require("./formatTimestamps");
+
+const customBlots = ["Timestamp"];
+
+const registerBlots = function(blotNames) {
+  blotNames.map(
+    function(blotName) {
+      const blotPath = `./../blots/${blotName}`;
+      const blot = require(blotPath);
+      Quill.register(blot);
+    },
+  );
+};
+
+const toolbarOptions = [
+  [
+    { size: ["small", false, "large", "huge"] }],  // custom dropdown
+  [
+    "clean"]];
+
+let transcriptEditor = new Quill(".transcript-editor", {
+  modules: {
+    toolbar: "#toolbar",
+  },
+  theme: "snow",
+  placeholder: "Transcribe away...",
+});
+
+formatTimestampsOnTextChange(transcriptEditor);
+registerBlots(customBlots);
+
+module.exports = transcriptEditor;
