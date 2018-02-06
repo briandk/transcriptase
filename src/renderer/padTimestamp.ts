0037466e121054b3a0edf9b93558459697850be6
Brian Danielak <briandaniela.k+github@gmail.com>
2018-02-06 18:00:38 -0500

    convert `padTimestamp` to typescript

---
+++ src/renderer/padTimestamp.ts
@@ -0,0 +1,10 @@
+export function padToHoursMinutesSeconds(timestamp) {
+  const shortTimestampPattern = /^\d{1,2}:\d\d$/;
+  const paddedTimestamp = `00:${timestamp}`;
+
+  if (shortTimestampPattern.test(timestamp)) {
+    return (paddedTimestamp);
+  } else {
+    return (timestamp);
+  }
+}
