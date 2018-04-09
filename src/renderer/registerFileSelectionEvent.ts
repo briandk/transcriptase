fdbb6b38c39c41789f6f0ee85ffe0e67f5dd67ce
Brian Danielak <briandaniela.k+github@gmail.com>
2018-04-09 18:17:31 -0400

    Reformat with Prettier

--- src/renderer/registerFileSelectionEvent.ts
+++ src/renderer/registerFileSelectionEvent.ts
@@ -1,7 +1,9 @@
 import { ipcRenderer as ipc } from "electron";
 
 export function registerFileSelectionEvent() {
-  const transcriptSelectionButton = document.getElementById("select-transcript-file");
+  const transcriptSelectionButton = document.getElementById(
+    "select-transcript-file",
+  );
   const mediaSelectionButton = document.getElementById("select-media-file");
 
   transcriptSelectionButton!.addEventListener("click", () => {
