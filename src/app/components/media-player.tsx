73b1a87a8cdf82ab5e9543245bc3ab1eae70c30f
Brian Danielak <briandaniela.k+github@gmail.com>
2018-07-19 20:39:04 -0400

    Refactor media player component

--- src/app/components/media-player.tsx
+++ src/app/components/media-player.tsx
@@ -1,17 +1,60 @@
 import React from "react"
+import videojs from "video.js"
+import { Player } from "video.js"
+import { Props } from "../../../node_modules/popmotion/lib/action/vector"
 
-class MediaPlayer extends React.Component<{}, {}> {
+// interface MediaContainerState {
+//   pathToMedia: string
+//   player: Player | null
+// }
+
+export class MediaContainer extends React.Component<{}, {}> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      pathToMedia: "",
+      player: null,
+    }
+  }
   render() {
-    return <h2>I'm a child!</h2>
+    return <MediaPlayer />
   }
 }
 
-export class MediaContainer extends React.Component<{}, {}> {
+interface MediaPlayerState {
+  player: Player
+}
+
+class MediaPlayer extends React.Component<{}, MediaPlayerState> {
+  player: Player
+  videoNode: Node
+
+  constructor(props: Props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    const videoNode = document.createElement("video")
+    this.player = videojs(videoNode, {
+      controls: true,
+      autoplay: false,
+      fluid: true,
+      playbackRates: [0.5, 0.75, 1, 1.25, 1.5, 2],
+    } as any)
+  }
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
   render() {
     return (
-      <div id="media-container">
-        <h1>This is the parent</h1>
-        <MediaPlayer />
+      <div>
+        <div data-vjs-player>
+          <video ref={node => (this.videoNode = node)} className="video-js" />
+        </div>
       </div>
     )
   }
