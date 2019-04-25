587a5753e318ad5874747dd7eecaf088e4ab1f6a
Brian A. Danielak <briandaniela.k+github@gmail.com>
2019-04-25 16:40:46 -0400

    Fix linting errors in saveBeforeClosing

--- src/main/saveBeforeClosing.ts
+++ src/main/saveBeforeClosing.ts
@@ -2,33 +2,50 @@ import { App, BrowserWindow, dialog, Event as ElectronEvent } from "electron"
 import { saveTranscript } from "./saveFile"
 import { getAppState, setAppState } from "../common/appState"
 
-export const rememberToSaveBeforeClosing = (window: BrowserWindow, app: App) => {
-  window.on("close", (event: ElectronEvent) => {
-    setAppState("userWantsToQuit", true)
-    event.preventDefault()
-    dialog.showMessageBox(
-      window,
-      {
-        buttons: ["Save, Then Quit", "Go Back to Editing", "Quit Without Saving"],
-        cancelId: 1,
-        type: "question",
-        defaultId: 0,
-        title: "Going so soon?",
-        message: "Do you want to save your changes before quitting?",
-      },
-      (response: number, checkboxChecked: boolean) => {
-        if (response === 0) {
-          saveTranscript(window, getAppState("transcript"), () => {
-            if (getAppState("userWantsToQuit") === true && getAppState("safeToQuit") === true) {
-              app.exit()
-            }
-          })
-        } else if (response === 1) {
-          return null
-        } else if (response === 2) {
-          app.exit()
-        }
-      },
-    )
-  })
+export const rememberToSaveBeforeClosing = (
+  window: BrowserWindow,
+  app: App,
+): void => {
+  window.on(
+    "close",
+    (event: ElectronEvent): void => {
+      setAppState("userWantsToQuit", true)
+      event.preventDefault()
+      dialog.showMessageBox(
+        window,
+        {
+          buttons: [
+            "Save, Then Quit",
+            "Go Back to Editing",
+            "Quit Without Saving",
+          ],
+          cancelId: 1,
+          type: "question",
+          defaultId: 0,
+          title: "Going so soon?",
+          message: "Do you want to save your changes before quitting?",
+        },
+        (response: number): void => {
+          if (response === 0) {
+            saveTranscript(
+              window,
+              getAppState("transcript"),
+              (): void => {
+                if (
+                  getAppState("userWantsToQuit") === true &&
+                  getAppState("safeToQuit") === true
+                ) {
+                  app.quit()
+                }
+              },
+            )
+          } else if (response === 1) {
+            null
+          } else if (response === 2) {
+            app.exit()
+          }
+        },
+      )
+    },
+  )
 }
