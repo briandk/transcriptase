2f7f092e91bf976701f97e58d216ff88333046e7
Brian Danielak <briandaniela.k+github@gmail.com>
2019-09-23 13:53:39 -0400

    Make saveBeforeClosing synchronous
    Electron v. 6.0 introduced a sort-of breaking change: dialog functions now return promises by default. They used to take a callback function.
    
    The breaking change means there are now explicitly synchronous versions of dialog methods—in this case: showMessageBoxSync.
    
    Refs https://github.com/electron/electron/pull/17054

--- src/main/saveBeforeClosing.ts
+++ src/main/saveBeforeClosing.ts
@@ -2,50 +2,42 @@ import { App, BrowserWindow, dialog, Event as ElectronEvent } from "electron"
 import { saveTranscript } from "./saveFile"
 import { getAppState, setAppState } from "../common/appState"
 
+const promptUserToSave = (
+  response: number,
+  app: App,
+  window: BrowserWindow,
+): void => {
+  if (response === 0) {
+    saveTranscript(window, getAppState("transcript"), (): void => {
+      if (
+        getAppState("userWantsToQuit") === true &&
+        getAppState("safeToQuit") === true
+      ) {
+        app.quit()
+      }
+    })
+  } else if (response === 1) {
+    null
+  } else if (response === 2) {
+    app.exit()
+  }
+}
+
 export const rememberToSaveBeforeClosing = (
   window: BrowserWindow,
   app: App,
 ): void => {
-  window.on(
-    "close",
-    (event: ElectronEvent): void => {
-      setAppState("userWantsToQuit", true)
-      event.preventDefault()
-      dialog.showMessageBox(
-        window,
-        {
-          buttons: [
-            "Save, Then Quit",
-            "Go Back to Editing",
-            "Quit Without Saving",
-          ],
-          cancelId: 1,
-          type: "question",
-          defaultId: 0,
-          title: "Going so soon?",
-          message: "Do you want to save your changes before quitting?",
-        },
-        (response: number): void => {
-          if (response === 0) {
-            saveTranscript(
-              window,
-              getAppState("transcript"),
-              (): void => {
-                if (
-                  getAppState("userWantsToQuit") === true &&
-                  getAppState("safeToQuit") === true
-                ) {
-                  app.quit()
-                }
-              },
-            )
-          } else if (response === 1) {
-            null
-          } else if (response === 2) {
-            app.exit()
-          }
-        },
-      )
-    },
-  )
+  window.on("close", (event: ElectronEvent): void => {
+    setAppState("userWantsToQuit", true)
+    event.preventDefault()
+    const userChoice = dialog.showMessageBoxSync(window, {
+      buttons: ["Save, Then Quit", "Go Back to Editing", "Quit Without Saving"],
+      cancelId: 1,
+      type: "question",
+      defaultId: 0,
+      title: "Going so soon?",
+      message: "Do you want to save your changes before quitting?",
+    })
+    promptUserToSave(userChoice, app, window)
+  })
 }
