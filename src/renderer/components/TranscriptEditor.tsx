0044d2ff77cca199ee8da3a7d66184f4754db57c
Brian Danielak <briandaniela.k+github@gmail.com>
2019-10-31 19:30:46 -0400

    Fix TypeError
    I don't even know what to say about this one. I ran around trying to down-convert a bunch of stuff to JavaScript and use React Hooks; for days it didn't work; I reconverted stuff to TypeScript; now it works.

---
+++ src/renderer/components/TranscriptEditor.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import Plain from "slate-plain-serializer"
+import { Editor } from "slate-react"
+
+const TranscriptEditor = (props: any): any => {
+  const placeholderText = `Drag a transcript here, or just type!`
+  //   return React.createElement(Editor, { className: "bar" })
+  return (
+    <div
+      id="editor-container"
+      //   className={this.state.classNames}
+      //   onDragOver={this.handleDragOver}
+      //   onDrop={this.handleDrop}
+    >
+      <Editor
+        placeholder={placeholderText}
+        value={Plain.deserialize(placeholderText)}
+        // ref={this.editorRef}
+        //   onChange={this.onChange as any}
+        //   renderDecoration={this.renderDecoration as any}
+        //   decorateNode={decorateNode as any}
+      />
+    </div>
+  )
+}
+
+export { TranscriptEditor }
