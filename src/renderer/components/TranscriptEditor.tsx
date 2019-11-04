879df532091f3b803e6adb45cb092c85b2ef1f38
Brian Danielak <briandaniela.k+github@gmail.com>
2019-11-04 15:29:20 -0500

    Broken react hooks
    The React Element for the transcript errors once you click it off of a fresh launch

--- src/renderer/components/TranscriptEditor.tsx
+++ src/renderer/components/TranscriptEditor.tsx
@@ -1,28 +1,48 @@
 import { ipcRenderer } from "electron"
-import React from "react"
+import { List } from "immutable"
+import React, { useState } from "react"
+import { Value, Operation } from "slate"
 import Plain from "slate-plain-serializer"
 import { Editor } from "slate-react"
 
-import { getThisTranscriptPlease } from "../../common/ipcChannelNames"
+import { getAppState, setAppState } from "../../common/appState"
+import {
+  getThisTranscriptPlease,
+  heresTheTranscript,
+} from "../../common/ipcChannelNames"
 
-function handleDragOver(event: any): void {
+const handleDragOver = (event: any): void => {
   event.dataTransfer.dropEffect = "link"
 }
 
-function handleDrop(event: any): void {
+const handleDrop = (event: any): void => {
   const path = event.dataTransfer.files[0].path
   ipcRenderer.send(getThisTranscriptPlease, path)
 }
 
+const writeTranscriptToLocalStorage = (transcript: string): void => {
+  localStorage.setItem("transcript", transcript)
+}
+
 const TranscriptEditor = (props: {}) => {
   const placeholderText = `Drag a transcript here, or just type!`
+  const [transcriptValue, setTranscriptValue] = useState(placeholderText)
   return (
     <div id="editor-card" onDragOver={handleDragOver} onDrop={handleDrop}>
       <Editor
         placeholder={placeholderText}
-        value={Plain.deserialize(placeholderText)}
+        value={Plain.deserialize(transcriptValue)}
         // ref={this.editorRef}
-        //   onChange={this.onChange as any}
+        onChange={(change: { operations: any; value: any }): void => {
+          const transcript = Plain.serialize(change.value)
+          if (change.value) {
+            setTranscriptValue(change.value)
+          }
+          // setAppState("transcript", transcript)
+          // setAppState("safeToQuit", false)
+          // writeTranscriptToLocalStorage(transcript)
+          // ipcRenderer.send(heresTheTranscript, Plain.serialize(change.value))
+        }}
         //   renderDecoration={this.renderDecoration as any}
         //   decorateNode={decorateNode as any}
       />
