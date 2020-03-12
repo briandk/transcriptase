0c8930b8856854621fa32603a13058e671ca0883
Brian Danielak <briandaniela.k+github@gmail.com>
2020-03-12 15:27:52 -0400

    Upgrade Electron tro 8.x

--- src/renderer/contentSecurityPolicy.ts
+++ src/renderer/contentSecurityPolicy.ts
@@ -1,8 +1,8 @@
-import { session, OnResponseStartedListenerDetails } from "electron"
+import { session, OnHeadersReceivedListenerDetails } from "electron"
 
 export const setContentSecurityPolicy = (): void => {
   session.defaultSession.webRequest.onHeadersReceived(
-    (details: OnResponseStartedListenerDetails, callback: Function): void => {
+    (details: OnHeadersReceivedListenerDetails, callback: Function): void => {
       callback({ responseHeaders: `default-src 'none'` })
     },
   )
