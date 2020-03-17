0aa7fb5ac12e7871bc8f16fd8899374194773416
Brian Danielak <briandaniela.k+github@gmail.com>
2020-03-17 16:02:36 -0400

    Get Editor mockup working

--- src/renderer/components/TranscriptEditor.tsx
+++ src/renderer/components/TranscriptEditor.tsx
@@ -1,11 +1,6 @@
-import PrismMarkdown from "../prism-markdown/prism-markdown"
-import React, { useState, useCallback, useMemo } from "react"
-import { Slate, Editable, withReact } from "slate-react"
-import { Text, createEditor } from "slate"
-import { withHistory } from "slate-history"
-import { css } from "emotion"
-
-PrismMarkdown
+import React, { useState, useMemo } from "react"
+// import { Slate, Editable, withReact } from "slate-react"
+// import { createEditor} from "slate"
 
 const initialValue = [
   {
@@ -24,118 +19,20 @@ const initialValue = [
   },
 ]
 
-export const MarkdownPreviewEditor = (): any => {
-  const [value, setValue] = useState(initialValue)
-  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
-  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
-  const decorate = useCallback(([node, path]) => {
-    const ranges: any[] = []
+const MarkdownPreviewEditor = (): any => {
+  // const editor = useMemo(() => withReact(createEditor()), [])
+  // const [value, setValue] = useState(initialValue)
 
-    if (!Text.isText(node)) {
-      return ranges
-    }
-
-    interface Subtoken {
-      length: number
-      content: Token[] | string
-    }
-    type Token = string | Subtoken
-    const getLength = (token: Token): number => {
-      if (typeof token === "string") {
-        return token.length
-      } else if (typeof token.content === "string") {
-        return token.content.length
-      } else {
-        return token.content.reduce(
-          (l: number, t: Token) => l + getLength(t),
-          0,
-        )
-      }
-    }
-
-    const tokens = PrismMarkdown.tokenize(
-      node.text,
-      PrismMarkdown.languages.markdown,
-    )
-    let start = 0
-
-    for (const token of tokens) {
-      const length = getLength(token)
-      const end = start + length
-
-      if (typeof token !== "string") {
-        ranges.push({
-          [token.type]: true,
-          anchor: { path, offset: start },
-          focus: { path, offset: end },
-        })
-      }
-
-      start = end
-    }
-
-    return ranges
-  }, [])
-
-  return (
-    <Slate
-      editor={editor}
-      value={value}
-      onChange={(value: any): void => setValue(value)}
-    >
-      <Editable
-        decorate={decorate}
-        renderLeaf={renderLeaf}
-        placeholder="Write some markdown..."
-      />
-    </Slate>
-  )
+  // return (
+  //   <Slate
+  //     editor={editor}
+  //     value={value}
+  //     onChange={(value: any): void => setValue(value)}
+  //   >
+  //     <Editable></Editable>
+  //   </Slate>
+  // )
+  return <h1>Hello, World!</h1>
 }
 
-const Leaf = ({ attributes, children, leaf }: any): any => {
-  return (
-    <span
-      {...attributes}
-      className={css`
-        font-weight: ${leaf.bold && "bold"};
-        font-style: ${leaf.italic && "italic"};
-        text-decoration: ${leaf.underlined && "underline"};
-        ${leaf.title &&
-          css`
-            display: inline-block;
-            font-weight: bold;
-            font-size: 20px;
-            margin: 20px 0 10px 0;
-          `}
-        ${leaf.list &&
-          css`
-            padding-left: 10px;
-            font-size: 20px;
-            line-height: 10px;
-          `}
-        ${leaf.hr &&
-          css`
-            display: block;
-            text-align: center;
-            border-bottom: 2px solid #ddd;
-          `}
-        ${leaf.blockquote &&
-          css`
-            display: inline-block;
-            border-left: 2px solid #ddd;
-            padding-left: 10px;
-            color: #aaa;
-            font-style: italic;
-          `}
-        ${leaf.code &&
-          css`
-            font-family: monospace;
-            background-color: #eee;
-            padding: 3px;
-          `}
-      `}
-    >
-      {children}
-    </span>
-  )
-}
+export { MarkdownPreviewEditor }
