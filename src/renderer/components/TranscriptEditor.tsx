ad9faacd79f257a70d0e0acb6d399e3aa805e3d5
Brian Danielak <briandaniela.k+github@gmail.com>
2020-05-26 16:53:06 -0400

    Stub out transcript-loading logic
    in the editor

--- src/renderer/components/TranscriptEditor.tsx
+++ src/renderer/components/TranscriptEditor.tsx
@@ -1,16 +1,39 @@
-import React, { useMemo, useState } from "react"
+import { userHasChosenTranscriptFile } from "../../common/ipcChannelNames"
+import { ipcRenderer, IpcRendererEvent } from "electron"
+import React, { useMemo, useState, useEffect } from "react"
 import { createEditor, Node } from "slate"
 import { Slate, Editable, withReact } from "slate-react"
+
+function loadTranscript(event: IpcRendererEvent): void {
+  console.log(event)
+}
+
+function subscribeToLoadingTranscript(): void {
+  ipcRenderer.on(userHasChosenTranscriptFile, (event) =>
+    console.log("I'm subscribed to transcript load events", event),
+  )
+}
+
+function unsubscribeFromLoadingTranscript(): void {
+  ipcRenderer.removeListener(userHasChosenTranscriptFile, loadTranscript)
+}
 
 function App(): JSX.Element {
   const editor = useMemo(() => withReact(createEditor()), [])
   // Add the initial value when setting up our state.
-  const [value, setValue] = useState<Node[]>([
+  const initialValue: Node[] = [
     {
       type: "paragraph",
       children: [{ text: "" }],
     },
-  ])
+  ]
+  const [value, setValue] = useState<Node[]>(initialValue)
+  useEffect(() => {
+    ipcRenderer.on(userHasChosenTranscriptFile, loadTranscript)
+    return function cleanup(): void {
+      unsubscribeFromLoadingTranscript()
+    }
+  })
 
   return (
     <Slate
