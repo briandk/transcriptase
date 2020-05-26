887f53630c3c5d9e84934519aa5020521842b348
Brian Danielak <briandaniela.k+github@gmail.com>
2020-05-26 19:08:24 -0400

    Serialize transcript as pseudo-HTML

--- src/renderer/serialization.ts
+++ src/renderer/serialization.ts
@@ -1,4 +1,4 @@
-import { Node } from "slate"
+import { Node, Text } from "slate"
 
 export function deserializeTranscript(transcript: string): Node[] {
   const value = transcript.split("\n").map((s) => {
@@ -11,6 +11,29 @@ export function deserializeTranscript(transcript: string): Node[] {
   return value
 }
 
+// From the slate Docs: https://docs.slatejs.org/concepts/09-serializing#html
+const serialize = (node: Node): any => {
+  if (Text.isText(node)) {
+    return node.text
+  }
+
+  const children: any = node.children.map((n) => serialize(n)).join("")
+
+  switch (node.type) {
+    case "quote":
+      return `<blockquote><p>${children}</p></blockquote>`
+    case "paragraph":
+      return `<p>${children}</p>`
+    case "link":
+      return `<a href="${node.url}">${children}</a>`
+    default:
+      return children
+  }
+}
+
 export function serializeTranscript(value: Node[]): string {
-  return value.join("\n\n")
+  const serializedTranscript = value.map((v) => serialize(v)).join("\n\n")
+  console.log("The serialized transcript is", serializedTranscript)
+  return serializedTranscript
+  //   return value.map((v) => serialize(v)).join("\n\n")
 }
