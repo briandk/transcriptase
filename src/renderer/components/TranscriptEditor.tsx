4570ab92e76b3b087758d279c5cdeda830f81b5d
Brian Danielak <briandaniela.k+github@gmail.com>
2020-06-13 16:04:01 -0400

    Remove Prism Markdown
    We're going to try the `markdown-it` library instead

--- src/renderer/components/TranscriptEditor.tsx
+++ src/renderer/components/TranscriptEditor.tsx
@@ -5,8 +5,6 @@ import {
 
 import { css } from "emotion"
 import { ipcRenderer } from "electron"
-import Prism from "prismjs"
-// import Markdown from "../prism-markdown/prism-markdown"
 import React, {
   useCallback,
   useMemo,
@@ -15,11 +13,10 @@ import React, {
   ReactElement,
 } from "react"
 import { deserializeTranscript, serializeTranscript } from "../serialization"
-import { createEditor, Node, Text } from "slate"
+import { createEditor, Node } from "slate"
 import { Slate, Editable, withReact } from "slate-react"
 
 // eslint-disable-next-line
-;Prism.languages.markdown=Prism.languages.extend("markup",{}),Prism.languages.insertBefore("markdown","prolog",{blockquote:{pattern:/^>(?:[\t ]*>)*/m,alias:"punctuation"},code:[{pattern:/^(?: {4}|\t).+/m,alias:"keyword"},{pattern:/``.+?``|`[^`\n]+`/,alias:"keyword"}],title:[{pattern:/\w+.*(?:\r?\n|\r)(?:==+|--+)/,alias:"important",inside:{punctuation:/==+$|--+$/}},{pattern:/(^\s*)#+.+/m,lookbehind:!0,alias:"important",inside:{punctuation:/^#+|#+$/}}],hr:{pattern:/(^\s*)([*-])([\t ]*\2){2,}(?=\s*$)/m,lookbehind:!0,alias:"punctuation"},list:{pattern:/(^\s*)(?:[*+-]|\d+\.)(?=[\t ].)/m,lookbehind:!0,alias:"punctuation"},"url-reference":{pattern:/!?\[[^\]]+\]:[\t ]+(?:\S+|<(?:\\.|[^>\\])+>)(?:[\t ]+(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\)))?/,inside:{variable:{pattern:/^(!?\[)[^\]]+/,lookbehind:!0},string:/(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\))$/,punctuation:/^[\[\]!:]|[<>]/},alias:"url"},bold:{pattern:/(^|[^\\])(\*\*|__)(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^\*\*|^__|\*\*$|__$/}},italic:{pattern:/(^|[^\\])([*_])(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^[*_]|[*_]$/}},url:{pattern:/!?\[[^\]]+\](?:\([^\s)]+(?:[\t ]+"(?:\\.|[^"\\])*")?\)| ?\[[^\]\n]*\])/,inside:{variable:{pattern:/(!?\[)[^\]]+(?=\]$)/,lookbehind:!0},string:{pattern:/"(?:\\.|[^"\\])*"(?=\)$)/}}}}),Prism.languages.markdown.bold.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.italic.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.bold.inside.italic=Prism.util.clone(Prism.languages.markdown.italic),Prism.languages.markdown.italic.inside.bold=Prism.util.clone(Prism.languages.markdown.bold); // prettier-ignore
 
 const initialTranscriptValue: Node[] = [
   {
@@ -28,9 +25,9 @@ const initialTranscriptValue: Node[] = [
   },
 ]
 
-function loadTranscriptFromFile(transcript: string): Node[] {
-  return deserializeTranscript(transcript)
-}
+// function loadTranscriptFromFile(transcript: string): Node[] {
+//   return deserializeTranscript(transcript)
+// }
 
 function TranscriptEditor(): JSX.Element {
   const editor = useMemo(() => withReact(createEditor()), [])
@@ -51,42 +48,6 @@ function TranscriptEditor(): JSX.Element {
       default:
         return <p {...attributes}>{children}</p>
     }
-  }, [])
-  const decorate = useCallback(([node, path]) => {
-    const ranges: any[] = []
-
-    if (!Text.isText(node)) {
-      return ranges
-    }
-
-    const getLength = (token: any): number => {
-      if (typeof token === "string") {
-        return token.length
-      } else if (typeof token.content === "string") {
-        return token.content.length
-      } else {
-        return token.content.reduce((l: number, t: any) => l + getLength(t), 0)
-      }
-    }
-    const tokens = Prism.tokenize(node.text, Prism.languages.markdown)
-    let start = 0
-
-    for (const token of tokens) {
-      const length = getLength(token)
-      const end = start + length
-
-      if (typeof token !== "string") {
-        ranges.push({
-          [token.type]: true,
-          anchor: { path, offset: start },
-          focus: { path, offset: end },
-        })
-      }
-
-      start = end
-    }
-    console.log("The Ranges are", ranges)
-    return ranges
   }, [])
 
   // listen for load events; clean up listeners when component will unmount
@@ -113,7 +74,6 @@ function TranscriptEditor(): JSX.Element {
       }}
     >
       <Editable
-        decorate={decorate}
         placeholder="Drag a text file here to open it, or just start typing!"
         renderElement={renderElement}
         renderLeaf={renderLeaf}
