361255f8035a7639056532ce14dd91c04d4f7cfa
Brian Danielak <briandaniela.k+github@gmail.com>
2020-09-27 16:38:53 -0400

    Remove unused is-hotkey dependency
    Also remove unused UIkit dependency

--- src/renderer/components/TranscriptEditor.tsx
+++ src/renderer/components/TranscriptEditor.tsx
@@ -3,15 +3,8 @@ import {
   userHasChosenTranscriptFile,
 } from "../../common/ipcChannelNames"
 
-import { css } from "emotion"
 import { ipcRenderer } from "electron"
-import React, {
-  useCallback,
-  useMemo,
-  useState,
-  useEffect,
-  ReactElement,
-} from "react"
+import React, { useCallback, useMemo, useState, useEffect } from "react"
 import { deserializeTranscript, serializeTranscript } from "../serialization"
 import { createEditor, Node } from "slate"
 import { Slate, Editable, withReact } from "slate-react"
@@ -32,7 +25,6 @@ const initialTranscriptValue: Node[] = [
 function TranscriptEditor(): JSX.Element {
   const editor = useMemo(() => withReact(createEditor()), [])
   const [value, setValue] = useState<Node[]>(initialTranscriptValue)
-  const renderLeaf = useCallback((props) => <Leaf {...props} />, [])
   const renderElement = useCallback(({ attributes, children, element }) => {
     switch (element.type) {
       case "episodeTitle":
@@ -76,66 +68,8 @@ function TranscriptEditor(): JSX.Element {
       <Editable
         placeholder="Drag a text file here to open it, or just start typing!"
         renderElement={renderElement}
-        renderLeaf={renderLeaf}
       />
     </Slate>
-  )
-}
-
-const Leaf = ({ attributes, children, leaf }: any): ReactElement => {
-  return (
-    <span
-      {...attributes}
-      className={css`
-        font-weight: ${leaf.bold && "bold"};
-        font-style: ${leaf.italic && "italic"};
-        text-decoration: ${leaf.underlined && "underline"};
-        ${
-          leaf.title &&
-          css`
-            display: inline-block;
-            font-weight: bold;
-            font-size: 20px;
-            margin: 20px 0 10px 0;
-          `
-        }
-        ${
-          leaf.list &&
-          css`
-            padding-left: 10px;
-            font-size: 20px;
-            line-height: 10px;
-          `
-        }
-        ${
-          leaf.hr &&
-          css`
-            display: block;
-            text-align: center;
-            border-bottom: 2px solid #ddd;
-          `
-        }
-        ${
-          leaf.blockquote &&
-          css`
-            display: inline-block;
-            border-left: 2px solid #ddd;
-            padding-left: 10px;
-            color: #aaa;
-            font-style: italic;
-          `
-        }
-        ${
-          leaf.code &&
-          css`
-            font-family: monospace;
-            padding: 3px;
-          `
-        }
-      `}
-    >
-      {children}
-    </span>
   )
 }
 
